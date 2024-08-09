0afb56abcef5a1fc76eba057913c55033b342fc1
goodtam8 <21225133@life.hkbu.edu.hk>
2024-08-09 16:31:27 +0800

    implement the font-end logic of submitting the register

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fyp_mobile/property/button.dart';
 import 'package:fyp_mobile/property/icon.dart';
+import 'package:http/http.dart' as http;
+
+class CustomRadioColor extends WidgetStateColor {
+  static const int _defaultColor = 0xFF4a75a5;
+  const CustomRadioColor() : super(_defaultColor);
+
+  @override
+  Color resolve(Set<WidgetState> states) {
+    if (states.contains(WidgetState.selected)) {
+      return const Color(_defaultColor);
+    }
+    return Colors.white; // Change this color as needed for unselected state
+  }
+}
+
+const WidgetStateColor customRadioColor = CustomRadioColor();
+GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -14,16 +33,38 @@ class _RegisterState extends State<Register> {
   TextEditingController name = TextEditingController();
   TextEditingController phone_num = TextEditingController();
   TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
   List<String> gender = ["Men", "Women"];
   String currentOption = "Men";
 
-void submitregister(){
+  Future<dynamic> submitregister() async {
+    if (_formKey.currentState!.validate()) {
+      Map<String, dynamic> data = {
+        'staff_id': int.parse(staff_id.text),
+        'name': name.text,
+        'pw': password.text,
+        'gender': currentOption,
+        'phone': int.parse(phone_num.text),
+      };
+      try {
+        var response = await http.post(
+            Uri.parse('http://10.0.2.2:3000/api/teacher/'),
+            headers: {'Content-Type': 'application/json'},
+            body: jsonEncode(data));
 
-}
+        print(response.body);
+        return (response.statusCode);
+      } catch (e) {
+        print(e);
+        return e.toString();
+      }
+    }
+    ;
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -48,111 +89,215 @@ void submitregister(){
       ),
       body: Padding(
         padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Staff ID',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: staff_id,
-              decoration: const InputDecoration(
-                  hintText: "Enter your staff_ID",
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Text(
-              'Name',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: name,
-              decoration: const InputDecoration(
-                  hintText: "Enter your Name", border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Text(
-              'Email',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: email,
-              decoration: const InputDecoration(
-                  hintText: "Enter your Email", border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Text(
-              'Phone Number',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: phone_num,
-              decoration: const InputDecoration(
-                  hintText: "Enter your Phone Number",
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Text(
-              'Gender',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: ListTile(
-                    title: const Text('Men'),
-                    leading: Radio<String>(
-                      value: gender[0],
-                      groupValue:currentOption,
-                      onChanged: (value) {
-                        setState(() {
-                          currentOption = value.toString();
-                        });
-                      },
-                    ),
-                  ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Staff ID',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                Expanded(
-                  child: ListTile(
-                     selectedColor: Color(0xFF4a75a5),
-                    title: const Text('Women'),
-                    leading: Radio<String>(
-                      value: gender[1],
-                      groupValue:currentOption,
-                      onChanged: (value) {
-                        setState(() {
-                          currentOption = value.toString();
-                        });
-                      },
-                    ),
-                  ),
+                const SizedBox(
+                  height: 8.0,
                 ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your Staff_ID'; // Validation error message
+                    } else if (int.tryParse(value) == null) {
+                      return 'Please input a valid staff_id';
+                    }
+                    return null; // Return null if the input is valid
+                  },
+                  controller: staff_id,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your staff_ID",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const Text(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your Name'; // Validation error message
+                    }
+                    return null;
+                  },
+                  controller: name,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your Name",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const Text(
+                  'Email',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your Email'; // Validation error message
+                    }
+                    return null;
+                  },
+                  controller: email,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your Email",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const Text(
+                  'Phone Number',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your Phone Number'; // Validation error message
+                    } else if (int.tryParse(value) == null) {
+                      return 'Please input a valid staff_id';
+                    }
+                    return null;
+                  },
+                  controller: phone_num,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your Phone Number",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const Text(
+                  'Password',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your Password'; // Validation error message
+                    }
+                    return null;
+                  },
+                  controller: password,
+                  decoration: const InputDecoration(
+                      hintText: "Enter your Password",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                const Text(
+                  'Gender',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: ListTile(
+                        title: const Text('Men'),
+                        leading: Radio<String>(
+                          value: gender[0],
+                          groupValue: currentOption,
+                          onChanged: (value) {
+                            setState(() {
+                              currentOption = value.toString();
+                            });
+                          },
+                          fillColor: WidgetStateProperty.resolveWith((states) {
+                            if (states.contains(WidgetState.selected)) {
+                              return customRadioColor;
+                            }
+                            return Colors
+                                .grey; // Change this color as needed for unselected state
+                          }),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        selectedColor: Color(0xFF4a75a5),
+                        title: const Text('Women'),
+                        leading: Radio<String>(
+                          value: gender[1],
+                          groupValue: currentOption,
+                          onChanged: (value) {
+                            setState(() {
+                              currentOption = value.toString();
+                            });
+                          },
+                          fillColor: WidgetStateProperty.resolveWith((states) {
+                            if (states.contains(WidgetState.selected)) {
+                              return customRadioColor;
+                            }
+                            return Colors
+                                .grey; // Change this color as needed for unselected state
+                          }),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Styled_button(
+                    onPressed: () async {
+                      var registermessage = await submitregister();
+                      if (registermessage != 201 &&
+                          _formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            behavior: SnackBarBehavior.floating,
+                            backgroundColor: Colors.transparent,
+                            elevation: 0,
+                            content: Container(
+                              padding: const EdgeInsets.all(16),
+                              height: 90,
+                              decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                color: Colors.red,
+                              ),
+                              child: const Row(children: [
+                                SizedBox(
+                                  width: 48.0,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Text("You have already register")
+                                    ],
+                                  ),
+                                ),
+                              ]),
+                            )));
+                      }
+                    },
+                    child: const Text("Register"))
               ],
             ),
-            Styled_button(onPressed: submitregister, child: const Text("Register"))
-          ],
+          ),
         ),
       ),
     );
