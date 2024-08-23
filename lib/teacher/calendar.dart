d611fcdc3a6e55d288104ab37aaaae037953c2f8
goodtam8 <21225133@life.hkbu.edu.hk>
2024-08-23 17:25:01 +0800

    implement the template of the weekly timetable

--- lib/teacher/calendar.dart
+++ lib/teacher/calendar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fyp_mobile/login.dart';
+import 'package:fyp_mobile/property/timetable.dart';
 import 'package:fyp_mobile/property/topbar.dart';
-import 'package:time_scheduler_table/time_scheduler_table.dart';
+import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -10,95 +13,146 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
-  List<EventModel> eventList = [
-    EventModel(
-      title: "Math",
-      columnIndex:
-          0, // columnIndex is columnTitle's index (Monday : 0 or Day 1 : 0)
-      rowIndex: 2, // rowIndex is rowTitle's index (08:00 : 0 or Time 1 : 0)
-      color: Colors.orange,
-    ),
-    EventModel(
-      title: "History",
-      columnIndex: 1,
-      rowIndex: 5,
-      color: Colors.pink,
-    ),
-    EventModel(
-      title: "Guitar & Piano Course",
-      columnIndex: 4,
-      rowIndex: 8,
-      color: Colors.green,
-    ),
-    EventModel(
-      title: "Meeting",
-      columnIndex: 3,
-      rowIndex: 1,
-      color: Colors.deepPurple,
-    ),
-    EventModel(
-      title: "Guitar and Piano Course",
-      columnIndex: 2,
-      rowIndex: 9,
-      color: Colors.blue,
-    )
+  late Future<String?> _tokenValue;
+
+  final List<String> daysOfWeek = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
   ];
-  TextEditingController textController = TextEditingController();
+  final List<String> timeSlots = [
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00'
+  ];
+
+  Future<Timetable> gettodayevent(String objectid) async {
+    var response = await http.get(
+        Uri.parse('http://10.0.2.2:3000/api/teacher/$objectid/timetable'),
+        headers: {'Content-Type': 'application/json'});
+    return parseTimetable(response.body);
+  }
+
+  @override
+  void initState() {
+    _tokenValue = storage.read(key: 'jwt');
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Topbar(),
-      body: TimeSchedulerTable(
-        cellHeight: 72,
-        cellWidth: 72,
-        columnTitles: [
-          "Mon",
-          "Tue",
-          "Wed",
-          "Thur",
-          "Fri",
-        ], // columnTitles is growable : you can add as much as you want
-        // columnTitles: const ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"],
-        // currentColumnTitleIndex: 2,      --> if currentColumnTitleIndex is 2 then selected day is 3.
-        currentColumnTitleIndex: DateTime.now().weekday - 1,
-        rowTitles: [
-          '09:00',
-          '10:00',
-          '11:00',
-          '12:00',
-          '13:00',
-          '14:00',
-          '15:00',
-          '16:00',
-          '17:00',
-          '18:00',
-        ],
-        // You can assign any value to rowTitles. For Example : ['1','2','3']
-        title: "2024 Semester 1 ",
-        titleStyle: const TextStyle(
-            fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),
-        eventTitleStyle: const TextStyle(color: Colors.white, fontSize: 8),
-        isBack: false, // back button
-        eventList: eventList,
-        scrollColor: Colors.deepOrange.withOpacity(0.7),
-        isScrollTrackingVisible: true,
-        eventAlert: EventAlert(
-          alertTextController: textController,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(12.0),
-          ),
-          addAlertTitle: "Add Event",
-          editAlertTitle: "Edit",
-          addButtonTitle: "ADD",
-          deleteButtonTitle: "DELETE",
-          updateButtonTitle: "UPDATE",
-          hintText: "Event Name",
-          textFieldEmptyValidateMessage: "Cannot be empty!",
-          updateOnPressed: (event) {}, // when an event updated from your list
-          deleteOnPressed: (event) {}, // when an event deleted from your list
+        appBar: AppBar(
+          title: Text('Weekly Timetable'),
         ),
-      ),
+        body: FutureBuilder<String?>(
+            future: _tokenValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/update');
+                    },
+                    child: const Text("Update"),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                Map<String, dynamic> decodedToken =
+                    JwtDecoder.decode(snapshot.data as String);
+                String oid = decodedToken["_id"].toString();
+
+                return FutureBuilder<Timetable>(
+                    future: gettodayevent(oid),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Timetable> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator(); // Show a loading indicator while waiting
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.hasData) {
+                        return SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                    height: 58,
+                                    width: 50,
+                                    alignment: Alignment.center,
+                                    color: Colors.grey[300],
+                                  ),
+                                  ...timeSlots.map((time) => Container(
+                                        height: 58,
+                                        width: 50,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.grey),
+                                        ),
+                                        child: Text(time),
+                                      )),
+                                ],
+                              ),
+                              ...daysOfWeek.map((day) => Column(
+                                    children: [
+                                      Container(
+                                        height: 58,
+                                        width: 80,
+                                        alignment: Alignment.center,
+                                        color: Colors.blue[100],
+                                        child: Text(day),
+                                      ),
+                                      ...timeSlots.map((time) => Container(
+                                            height: 58,
+                                            width: 80,
+                                            alignment: Alignment.center,
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.grey),
+                                            ),
+                                            child:
+                                                _getEventForTimeSlot(day, time),
+                                          ))
+                                    ],
+                                  ))
+                            ],
+                          ),
+                        );
+                      } else {
+                        return Text("unexpected error occured");
+                      }
+                    });
+              }
+            }));
+  }
+}
+
+Widget _getEventForTimeSlot(String day, String time) {
+  // This is where you would implement your logic to display events
+  // For this example, we'll just display a sample event on Monday at 10:00
+  if (day == 'Mon' && time == '10:00') {
+    return Container(
+      color: Colors.green[100],
+      child: Text('Meeting'),
     );
   }
+  return Container();
+}
+
+Widget _gettime(String time){
+
 }
