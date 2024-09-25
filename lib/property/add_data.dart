4b3b5642c20b191cc6b2274600d61619f11fc088
Sun Tam <21225133@life.hkbu.edu.hk>
2024-09-25 16:34:02 +0800

    fix the bug of firebase storage

--- lib/property/add_data.dart
+++ lib/property/add_data.dart
@@ -3,34 +3,26 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-final FirebaseStorage _storage=FirebaseStorage.instance;
-final FirebaseFirestore _firestore=FirebaseFirestore.instance;
-class StoreData{
-Future<String> uploadimagetoStorage(String filename,Uint8List file)async{
-  Reference ref=_storage.ref().child(filename);
-  UploadTask uploadTask= ref.putData(file);
-  TaskSnapshot snapshot=await uploadTask;
-  String downlaodurl=await snapshot.ref.getDownloadURL();
-  return downlaodurl;
-}
-Future<String>saveData({required String id,required Uint8List file})
-async
-{  String resp="Some error occured";
+final FirebaseStorage _storage = FirebaseStorage.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  try {
+class StoreData {
+  Future<String> uploadimagetoStorage(String filename, Uint8List file) async {
+    Reference ref = _storage.ref().child(filename);
+    UploadTask uploadTask = ref.putData(file);
+    TaskSnapshot snapshot = await uploadTask;
+    String downlaodurl = await snapshot.ref.getDownloadURL();
+    return downlaodurl;
+  }
 
-  String imageUrl=await uploadimagetoStorage("profileImage", file);
-  await _firestore.collection('staffprofile').add({
-    'sid':id,
-    'imagelink':imageUrl
-  });
-  resp='success';
-} catch (e) {
-  resp=e.toString();
+  Future<String> saveData({required String id, required Uint8List file}) async {
+    String resp = "ok";
 
-}
-return resp;
-
-}
-
+    try {
+      String imageUrl = await uploadimagetoStorage(id, file);
+    } catch (e) {
+      resp = e.toString();
+    }
+    return resp;
+  }
 }
